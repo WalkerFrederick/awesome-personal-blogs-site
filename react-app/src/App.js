7e9107fbb8e522f452ab05254966eb0cc6d0cb46
walker <walker@urdev.xyz>
2019-03-18 17:48:34 -0400

    added favicons and footer

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 const Header = () => {
-  
   return (
     <nav className='App-header'>
       <a>Awesome Personal Blogs</a>
     </nav>
   )
+}
 
+const Footer = () => {
+  return (
+    <footer className='App-footer'>
+      <a target="_blank" href="https://github.com/jkup/awesome-personal-blogs">
+        Original List by Jon Kuperman
+      </a>
+      <a target="_blank" href="https://github.com/jkup/awesome-personal-blogs">
+        View the GitHub repo for this site.
+      </a>
+    </footer>
+  )
 }
 
 class App extends Component {
@@ -28,10 +39,15 @@ class App extends Component {
   render() {
     this.blogs = this.state.blogs.map((blog) => {
 
+      console.log(blog.url)
+
       return (
         <li className="list-item" key={blog.id}>
           <a href={blog.url} target={'_blank'}>
           
+          <img height="12" width="12" alt={`${blog.name}'s blog favicon`} src={`https://www.google.com/s2/favicons?domain=${blog.url}`} />
+
+
           {blog.name}
           
           </a>
@@ -46,6 +62,7 @@ class App extends Component {
        <ul className='list'>
           { this.blogs }
        </ul>
+       <Footer/>
       </div>
     );
   }
