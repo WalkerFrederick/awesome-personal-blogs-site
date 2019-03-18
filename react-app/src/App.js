ce60f1ba3757cea5db894a873f4e5b8537a72790
walker <walker@urdev.xyz>
2019-03-18 16:53:32 -0400

    blogs now populate to app.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const Header = () => {
+  
+  return (
+    <nav className='App-header'>
+      <a>Awesome Personal Blogs</a>
+    </nav>
+  )
+
+}
+
 class App extends Component {
-  state = {blogs: []}
+  constructor(props) {
+    super(props);
+
+    this.state = {blogs: []}
+
+  }
 
   componentDidMount() {
     fetch('/blogs')
       .then(res => res.json())
-      .then(blogs => console.log(blogs.blogs));
+      .then(blogs => this.setState({blogs: blogs.blogs}));
   }
 
   render() {
+    this.blogs = this.state.blogs.map((blog) => {
+
+      return (
+        <li className="list-item" key={blog.id}>
+          <a href={blog.url} target={'_blank'}>
+          
+          {blog.name}
+          
+          </a>
+        </li>
+      )
+  
+    })
+
     return (
       <div className="App">
-      
+      <Header/>
+       <ul className='list'>
+          { this.blogs }
+       </ul>
       </div>
     );
   }
 }
+
 
 export default App;
